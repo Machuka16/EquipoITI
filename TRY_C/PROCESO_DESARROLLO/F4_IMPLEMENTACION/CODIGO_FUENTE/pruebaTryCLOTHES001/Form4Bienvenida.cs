095feee58de8cb8256d02dcc3aab5d8b6ca2b693
Heresgo <7641@utcv.edu.mx>
2017-02-15 11:54:24 -0600

    Script base de datos y cambiar nombre de carpeta 15/02/2017

---
+++ TRY_C/PROCESO_DESARROLLO/F4_IMPLEMENTACION/CODIGO_FUENTE/pruebaTryCLOTHES001/Form4Bienvenida.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pruebaTryCLOTHES001
+{
+    public partial class Form4Bienvenida : Form
+    {
+        public Form4Bienvenida()
+        {
+            InitializeComponent();
+        }
+    }
+}
